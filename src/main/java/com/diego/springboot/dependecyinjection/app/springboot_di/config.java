a6625c6db5b295143c59cb5a606b208e8c1133de
Diagomezpa <Diagomezpa@gmail.com>
2026-02-25 23:27:36 -0500

    seccion 3 64. Algunas variantes en el componente registrado con @Configuration y @Bean parte 2

--- src/main/java/com/diego/springboot/dependecyinjection/app/springboot_di/config.java
+++ src/main/java/com/diego/springboot/dependecyinjection/app/springboot_di/config.java
@@ -26,6 +26,7 @@ public class config {
     // esta es una forma de registrar un bean en el contenedor de spring, es una forma manual de registrar un bean, se recomienda usar esta forma cuando queremos registrar un bean que no es una clase anotada con @Component, @Service, @Repository o @Controller, por ejemplo, si queremos registrar un bean que es una clase de una librería externa que no tiene anotaciones de spring, podemos usar esta forma para registrar ese bean en el contenedor de spring y luego inyectarlo en nuestros controladores o servicios sin necesidad de modificar el código de la clase de la librería externa, esto hace que el código sea más limpio y fácil de mantener, además, permite registrar beans con configuraciones específicas, por ejemplo, si queremos registrar un bean con un scope específico o con un nombre específico, podemos usar esta forma para configurar ese bean de acuerdo a nuestras necesidades
     @Bean("productJson")
     public ProductRepository productRepositoyJson() {
+        //return new ProductRepositoryJson(resource);
         return new ProductRepositoryJson();
     }
 
